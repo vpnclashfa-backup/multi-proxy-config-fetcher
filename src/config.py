40bc28535979356872b28e4c0dbb58d797908b92
Anonymous <159781143+4n0nymou3@users.noreply.github.com>
2025-03-02 10:34:55 -0800

    config.py

--- src/config.py
+++ src/config.py
@@ -69,10 +69,10 @@ class ProxyConfig:
         # Option 2: Set specific_config_count > 0 for desired number of configs (Default: 50)
         # Note: If use_maximum_power is True, specific_config_count will be ignored
         self.use_maximum_power = False
-        self.specific_config_count = 200
+        self.specific_config_count = 100
 
         initial_urls = [
-            ChannelConfig("https://raw.githubusercontent.com/4n0nymou3/wg-config-fetcher/refs/heads/main/configs/wireguard_configs.txt"),
+            # ChannelConfig("https://raw.githubusercontent.com/4n0nymou3/wg-config-fetcher/refs/heads/main/configs/wireguard_configs.txt"),
             ChannelConfig("https://raw.githubusercontent.com/4n0nymou3/ss-config-updater/refs/heads/main/configs.txt"),
             ChannelConfig("https://raw.githubusercontent.com/valid7996/Gozargah/refs/heads/main/Gozargah_Sub"),
             ChannelConfig("https://raw.githubusercontent.com/mahsanet/MahsaFreeConfig/refs/heads/main/mtn/sub_1.txt"),
@@ -101,7 +101,7 @@ class ProxyConfig:
 
     def _initialize_protocols(self) -> Dict:
         return {
-            "wireguard://": {"priority": 2, "aliases": [], "enabled": False},
+            # "wireguard://": {"priority": 1, "aliases": [], "enabled": True},
             "hysteria2://": {"priority": 2, "aliases": ["hy2://"], "enabled": True},
             "vless://": {"priority": 2, "aliases": [], "enabled": True},
             "vmess://": {"priority": 1, "aliases": [], "enabled": True},
