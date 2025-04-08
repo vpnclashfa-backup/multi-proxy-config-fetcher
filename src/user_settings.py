5ef22163f71e868e617296a5b892abd07f754da8
Tenium <amin.behravan@yahoo.com>
2025-04-08 08:10:36 +0330

    Update user_settings.py

--- src/user_settings.py
+++ src/user_settings.py
@@ -127,7 +127,7 @@ USE_MAXIMUM_POWER = False
 
 # Desired number of configurations to fetch.
 # This is used only if USE_MAXIMUM_POWER is False.
-SPECIFIC_CONFIG_COUNT = 100
+SPECIFIC_CONFIG_COUNT = 200
 
 # Dictionary of protocols to enable or disable.
 # Set each protocol to True to enable, False to disable.
